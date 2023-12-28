4e319c4a5c6865b46b7c20e7788dea137b640cc7
Mikhail Kramushchanka <s24124@pjwstk.edu.pl>
2023-12-28 01:46:34 +0100

    Initial commit

---
+++ src/main/java/com/company/ytlook/YtlookApplication.java
@@ -0,0 +1,13 @@
+package com.company.ytlook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtlookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtlookApplication.class, args);
+	}
+
+}
